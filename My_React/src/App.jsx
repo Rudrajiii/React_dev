6e8ca7c3137e192077c73ae9f1c86f783892d845
rudrajiii <rudrasaha305@gmail.com>
2024-07-26 22:32:14 +0530

    state example is added

--- My_React/src/App.jsx
+++ My_React/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { GojoComponent } from "./components/GojoComponent";
 import { EventHandler } from "./components/eventHandling";
+import {State} from "./components/StateManagement"
 
 
 export function App() {
   return (
     <section className="container w-full">
       <GojoComponent />
+      {/* <State/> */}
       {/* <EventHandler/> */}
     </section>
   );
---
+++ My_React/src/components/StateManagement.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+export function State() {
+  let myData = [
+      { name: "hi", age: 2983 },
+      { name: "Si", age: 2094 },
+      { name: "Rudra", age: 802 },
+      { name: "hihi", age: 293 },
+      { name: "Sikha", age: 243 },
+      { name: "ks", age: 233 },
+      { name: "Ts", age: 203 },
+      { name: "kshi", age: 93 },
+      { name: "oi", age: 83 },
+      { name: "Sakshi", age: 73 },
+      { name: "Shi", age: 63 },
+      { name: "Ankush", age: 60 },
+    ];  
+  const [publicDataset , _] = useState(myData);  
+  return (
+    <ul>
+      {publicDataset.map((elem, index) => {
+        return (
+          <>
+            <li key={`${elem.age}`}>
+              {elem.name} - {elem.age}
+            </li>
+          </>
+        );
+      })}
+    </ul>
+  );
+}
